8dfe3a8460135342ea511fb073cf78532cad169c
unknown <krboktv@gmail.com>
2017-11-11 18:29:15 +0300

    ШИФРУЕМ ПРИВАТНЫЙ КЛЮЧ НА EZ

--- Frontend/js/addWavesWallet.js
+++ Frontend/js/addWavesWallet.js
@@ -4,17 +4,20 @@ const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
 
 const seed = Waves.Seed.create();
 
-console.log(seed.keyPair); // Генерит ПРИВАТНЫЙ и ПУБЛИЧНЫЙ КЛЮЧ
+console.log(seed.keyPair.privateKey); // Генерит ПРИВАТНЫЙ и ПУБЛИЧНЫЙ КЛЮЧ
 
+function Encription() {
+    var serverString = $('input[name="serverString"]').val(); // Строка, которую надо скрестить с публичным адресом и зашифровать
+    var privateKey = $('input[name="privateKey"]').val();     // Строка с приватным ключом
+    
+    var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(privateKey), serverString); // Шифруем ключ + private key
+    
+    $('input[name=endKey]').val(ciphertext.toString());                     // Показываем шифрокод в 3-й строчке              
+    
+    var takeCipher = $('input[name=endKey]').val();                         // Получаем значение шифрокода
 
+    var bytes  = CryptoJS.AES.decrypt(takeCipher.toString(), serverString); // Дешифруем   
+    var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));      // В норм формат переводим
 
-var encrypted = CryptoJS.SHA256("Привет");
-
-
-// Encrypt
-var ciphertext = CryptoJS.AES.encrypt(JSON.stringify('Privet'), 'secret key 123');
-
-// Decrypt
-var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
-var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
-console.log(decryptedData);
+    console.log(decryptedData);
+    }
