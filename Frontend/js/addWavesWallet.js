9eff26b61be900036057566726911840e5b77c98
unknown <krboktv@gmail.com>
2017-11-11 17:13:05 +0300

    Добавил API. Добавил API криптожа

--- Frontend/js/addWavesWallet.js
+++ Frontend/js/addWavesWallet.js
@@ -1,4 +1,16 @@
+
+
 const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
 
 const seed = Waves.Seed.create();
-console.log(seed.phrase);
+
+console.log(seed.keyPair); // Генерит ПРИВАТНЫЙ и ПУБЛИЧНЫЙ КЛЮЧ
+
+
+
+var encrypted = CryptoJS.SHA256("Привет");
+console.log(encrypted);
+
+var ciphertext = CryptoJS.AES.encrypt("Privet", 'secret key 123');
+
+// var ciphertext = CryptoJS.AES.decrypt(JSON.stringify(data), 'secret key 123');
